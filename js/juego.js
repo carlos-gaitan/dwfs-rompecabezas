136496dc9b4b6d0fb175a0b758f4674ed8163edb
elcharly <ninguno@elcharly.com>
2018-06-12 12:53:26 -0300

    I start coding the modal css alert

--- js/juego.js
+++ js/juego.js
@@ -53,7 +53,40 @@ function chequearSiGano() {
 // Implementar alguna forma de mostrar un cartel que avise que ganaste el juego
 function mostrarCartelGanador() {
     //COMPLETAR
+    modal.style.display = "block";
+    if (span.onclick || window.onclick ) {
+      modal.style.display = "none";
+    }
+
     alert('Felicidades, has ganado!');
+    // TODO: Quiero hacer andar esto https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal o http://bootboxjs.com/
+    //     <script>
+    // // Get the modal
+    // var modal = document.getElementById('myModal');
+    //
+    // // Get the button that opens the modal
+    // var btn = document.getElementById("myBtn");
+    //
+    // // Get the <span> element that closes the modal
+    // var span = document.getElementsByClassName("close")[0];
+    //
+    // // When the user clicks the button, open the modal
+    // btn.onclick = function() {
+    //     modal.style.display = "block";
+    // }
+    //
+    // // When the user clicks on <span> (x), close the modal
+    // span.onclick = function() {
+    //     modal.style.display = "none";
+    // }
+    //
+    // // When the user clicks anywhere outside of the modal, close it
+    // window.onclick = function(event) {
+    //     if (event.target == modal) {
+    //         modal.style.display = "none";
+    //     }
+    // }
+    // </script>
 }
 
 /* Función que intercambia dos posiciones en la grilla.
