9d5a6fd4416c87ef39bb6d32d795ea7e521c6ca3
elcharly <ninguno@elcharly.com>
2018-06-12 21:05:12 -0300

    I finish the modal css alert

--- js/juego.js
+++ js/juego.js
@@ -11,6 +11,12 @@ var grilla = [
     [7, 8, 9]
 ];
 
+//para el Modal
+var modal = document.getElementById('myModal');
+modal.addEventListener('click', function(){
+  modal.style.display = "none";
+});
+
 /* Estas dos variables son para guardar la posición de la pieza vacía.
 Esta posición comienza siendo la [2, 2]*/
 var filaVacia = 2;
@@ -54,39 +60,6 @@ function chequearSiGano() {
 function mostrarCartelGanador() {
     //COMPLETAR
     modal.style.display = "block";
-    if (span.onclick || window.onclick ) {
-      modal.style.display = "none";
-    }
-
-    alert('Felicidades, has ganado!');
-    // TODO: Quiero hacer andar esto https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal o http://bootboxjs.com/
-    //     <script>
-    // // Get the modal
-    // var modal = document.getElementById('myModal');
-    //
-    // // Get the button that opens the modal
-    // var btn = document.getElementById("myBtn");
-    //
-    // // Get the <span> element that closes the modal
-    // var span = document.getElementsByClassName("close")[0];
-    //
-    // // When the user clicks the button, open the modal
-    // btn.onclick = function() {
-    //     modal.style.display = "block";
-    // }
-    //
-    // // When the user clicks on <span> (x), close the modal
-    // span.onclick = function() {
-    //     modal.style.display = "none";
-    // }
-    //
-    // // When the user clicks anywhere outside of the modal, close it
-    // window.onclick = function(event) {
-    //     if (event.target == modal) {
-    //         modal.style.display = "none";
-    //     }
-    // }
-    // </script>
 }
 
 /* Función que intercambia dos posiciones en la grilla.
